90ddcb26df43f373092468afadd2db5cb0ed7213
lmkaka <bidenclave@gmail.com>
2025-11-09 23:29:20 +0530

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -217,8 +217,8 @@ const Hero = () => {
                     {/* Trust Badges - Horizontal Scroll on Mobile */}
                     <div className="flex gap-2 overflow-x-auto pb-2 scrollbar-hide -mx-4 px-4 sm:mx-0 sm:px-0 sm:grid sm:grid-cols-3 sm:gap-3">
                       {[
-                        { icon: BadgeCheck, text: 'Lab Tested' }
-                        { icon: Shield, text: 'FSSAI Certified' },
+                        { icon: BadgeCheck, text: 'Lab Tested' },
+                        { icon: Shield, text: 'FSSAI Certified' }
                       
                       ].map((item, idx) => (
                         <div
